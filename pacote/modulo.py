b360f2917efa2d4b77e458504789ee5c0bf3eb91
alexandrearakaki <ale.miyagi@gmail.com>
2020-04-16 17:03:13 -0300

    aula4

---
+++ pacote/modulo.py
@@ -0,0 +1,9 @@
+def soma(*args):
+    return sum(args)
+
+if __name__ == '__main__':
+    print(__name__)
+    print(soma())
+    print(soma(1,2))
+    print(soma(3,4))
+    print(soma(5,6))
---
+++ pacote/novo_modulo.py
@@ -0,0 +1,3 @@
+from pacote.modulo import soma as s
+
+print(s(1,2,3))
